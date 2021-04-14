d4a7d7ff8f278b10d47c754854ea1e4fba8c3e38
splendor <negatifw@gmail.com>
2021-04-14 15:37:19 +0300

    add new design classes

--- src/main/java/com/soter/api/domain/Attributes.java
+++ src/main/java/com/soter/api/domain/Attributes.java
@@ -1,4 +1,10 @@
 package com.soter.api.domain;
 
+import com.soter.api.enums.AttributesType;
+
 public class Attributes {
+    private Long id;
+    private String name;
+    private AttributesType attributesType;
+    private double points;
 }
---
+++ src/main/java/com/soter/api/enums/AttributesType.java
@@ -0,0 +1,8 @@
+package com.soter.api.enums;
+
+public enum  AttributesType {
+    SPEED,
+    MAGICAL_ATTACK,
+    ATTACK_DAMAGE,
+    DEFENCE
+}
